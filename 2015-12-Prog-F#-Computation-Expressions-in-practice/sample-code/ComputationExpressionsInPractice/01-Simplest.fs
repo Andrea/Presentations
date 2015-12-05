8f9e470932b3f920d3e15c0aca69f8b5b7f82631
Andrea <roundcrisis@gmail.com>
2015-12-05 17:41:17 +0000

    simplest- remove unused reference

--- 2015-12-Prog-F#-Computation-Expressions-in-practice/sample-code/ComputationExpressionsInPractice/01-Simplest.fs
+++ 2015-12-Prog-F#-Computation-Expressions-in-practice/sample-code/ComputationExpressionsInPractice/01-Simplest.fs
@@ -1,18 +1,16 @@
 ﻿namespace ComputationExpressionsInPractice
 
-open NUnit.Framework
-
 module Simplest =
     
     type OnlyContinueBuilder () =
-        // apply x to f
+
         member this.Bind(x,f) = 
-          printfn "from Bind begin  %A %s " (System.DateTime.Now.TimeOfDay)  (x.ToString()) 
+          printfn "Bind begin  %A %s " (System.DateTime.Now.TimeOfDay)  (x.ToString()) 
           let y = f x
-          printfn "from Bind end  %A %s " (System.DateTime.Now.TimeOfDay)  (x.ToString()) 
+          printfn "Bind end  %A %s " (System.DateTime.Now.TimeOfDay)  (x.ToString()) 
           y
         member this.Return(x) = 
-            printfn "from return %A %s"  (System.DateTime.Now.TimeOfDay) (x.ToString())
+            printfn "Return %A %s"  (System.DateTime.Now.TimeOfDay) (x.ToString())
             x
 
     let onlyContinue = new OnlyContinueBuilder() 
